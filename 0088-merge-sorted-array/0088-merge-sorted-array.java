56d13592c34f8cf1d39d92e03dbbcf0cfd7cef45
harman5911 <114011871+harman5911@users.noreply.github.com>
2022-11-03 09:37:39 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (65.49%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -16,7 +16,11 @@ class Solution {
                 k--;
             }
         }
-      
+      while(i>=0){
+           nums1[k] = nums1[i];
+                i--;
+                k--;
+      }
         
         while(j>=0){
              nums1[k] = nums2[j];
