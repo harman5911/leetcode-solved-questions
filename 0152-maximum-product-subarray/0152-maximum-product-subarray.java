216fe3a4e2b97aafe47e08b08a3ce32cc171fdd3
harman5911 <114011871+harman5911@users.noreply.github.com>
2022-11-08 10:34:37 +0530

    Time: 276 ms (8.03%), Space: 42.7 MB (84.29%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,6 +3,7 @@ class Solution {
         int n = nums.length;
         int result = nums[0];
         
+        // Time Complexity - O(n2)
         for(int i=0;i<n;i++){
             int product = 1;
             for(int j=i;j<n;j++){
