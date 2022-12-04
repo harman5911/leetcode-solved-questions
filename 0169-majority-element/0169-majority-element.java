5a58cb7640cc5c1f9a397caa71178de6cc79416e
harman5911 <114011871+harman5911@users.noreply.github.com>
2022-12-04 09:56:17 +0530

    Time: 52 ms (8.25%), Space: 55.3 MB (71.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        /*
         int n = nums.length;
         int res = nums[0];
         int count =1;
@@ -16,5 +17,25 @@ class Solution {
             }
         }
         return res;
+        */
+        
+        // By using Hashmap
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        int ans =0;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        
+        for(int key : map.keySet()){
+            if(map.get(key) > n/2){
+                ans = key;
+            }
+        }
+        return ans;
     }
 }
