e7ceff93ec2e3b21a17ed070d37a5d9c6b91f97c
harman5911 <114011871+harman5911@users.noreply.github.com>
2023-03-18 13:13:33 +0530

    Added solution - LeetHub

---
+++ Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+        /*
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+        for(long i=0;i<A.length;i++){
+            if(hmap.containsKey(A[i])){
+                hmap.put(A[i],hmap.get(A[i])+1);
+            }else{
+                hmap.put(A[i],1);
+            }
+        }
+        
+        for(long i=0;i<B.length;i++){
+            if(hmap.containsKey(B[i])){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return true;
+        */
+            Arrays.sort(A);
+            Arrays.sort(B);
+            
+            return Arrays.equals(A,B);
+    }
+}
